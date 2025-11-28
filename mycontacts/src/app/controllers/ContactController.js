41c84547ee90a6c59d483e7d66570c385c0927a2
sophia-hashtag <sophia.vieceli@hashtagtreinamentos.com>
2025-11-28 09:42:40 -0300

    finalização das rotas do mycontacts usando uuid

--- mycontacts/src/app/controllers/ContactController.js
+++ mycontacts/src/app/controllers/ContactController.js
@@ -53,8 +53,37 @@ class ContactController {
         response.json(contact);
     }
 
-    update() {
+    async update(request, response) {
         // editar um registro
+
+        const { id } = request.params;
+        const { name, email, phone, category_id } = request.body;
+
+        const contactExists = await ContactsRepository.findById(id);
+
+        if (!contactExists) {
+            return response.status(404).json({ error: "User not found" });
+        }
+
+        if (!name) {
+            return response.status(400).json({ error: "Name is required" });
+        }
+
+        const contactByEmail = await ContactsRepository.findByEmail();
+        if (contactByEmail && contactByEmail.id !== id) {
+            return response
+                .status(400)
+                .json({ error: "This e-mail is already taken" });
+        }
+
+        const contact = await ContactsRepository.update(id, {
+            name,
+            email,
+            phone,
+            category_id,
+        });
+
+        response.json(contact);
     }
 
     async delete(request, response) {
--- mycontacts/src/app/repositories/ContactsRepository.js
+++ mycontacts/src/app/repositories/ContactsRepository.js
@@ -11,6 +11,7 @@ let uuid;
 let contacts = [
     {
         id: () => uuid(),
+        // id: "ee591024-167e-4e78-9471-d36444f668c3",
         name: "Sophia",
         email: "sophia@gmail.com",
         phone: "5100000000",
@@ -128,6 +129,46 @@ class ContactsRepository {
             contacts.push(newContact);
             resolve(newContact);
         });
+    }
+
+    update(id, { name, email, phone, category_id }) {
+        return new Promise((resolve) => {
+            const updatedContact = {
+                id,
+                name,
+                email,
+                phone,
+                category_id,
+            };
+
+            contacts = contacts.map((contact) =>
+                contact.id === id ? updatedContact : contact
+            );
+
+            resolve(updatedContact);
+        });
+
+        // return new Promise((resolve) => {
+        //     let newContact = {
+        //         id: () => uuid(),
+        //         name,
+        //         email,
+        //         phone,
+        //         category_id,
+        //     };
+
+        //     newContact = {
+        //         ...newContact,
+        //         id:
+        //             typeof newContact.id === "function"
+        //                 ? newContact.id()
+        //                 : newContact.id,
+        //         category_id:
+        //             typeof newContact.category_id === "function"
+        //                 ? newContact.category_id()
+        //                 : newContact.category_id,
+        //     };
+        // });
     }
 }
 
